1ed5b26aa131be03a9b6c1edfb7f86825a47529d
! Nobita studio <129251677+nobita586@users.noreply.github.com>
2025-09-08 12:09:34 +0530

    Update panel.sh
    Signed-off-by: ! Nobita studio  <129251677+nobita586@users.noreply.github.com>

--- cd/panel.sh
+++ cd/panel.sh
@@ -26,7 +26,7 @@ echo "deb [signed-by=/usr/share/keyrings/redis-archive-keyring.gpg] https://pack
 apt update
 
 # --- Install PHP + extensions ---
-apt install -y php8.2 php8.2-{cli,fpm,common,mysql,mbstring,bcmath,xml,zip,curl,gd,tokenizer,ctype,simplexml,dom} mariadb-server nginx redis-server
+apt install -y php8.3 php8.3-{cli,fpm,common,mysql,mbstring,bcmath,xml,zip,curl,gd,tokenizer,ctype,simplexml,dom} mariadb-server nginx redis-server
 
 # --- Install Composer ---
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
